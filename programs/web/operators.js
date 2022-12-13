fdd98a425c6865a92aee22145b954dcd676df7dc
raghu prasad <prasadraghuks@gmail.com>
2022-12-13 12:13:58 +0530

    updated

---
+++ programs/web/operators.js
@@ -0,0 +1,23 @@
+console.log('operators')
+//Operators - Arithmetic,Comparision,logical
+//arithmetic +,-,*,/,%,**,++,--
+console.log('Arithmetic operators')
+n1 = 100
+n2 = 200
+res = n1+n2
+console.log(n1+' + '+n2+' = ',res)
+console.log('comparision operators')
+// Comparision >,<,>=,<=,==,!=,===
+n3='200'
+console.log(n2+' == '+n3+' = ',(n2==n3))
+console.log(n2+' === '+n3+' = ',(n2===n3))
+console.log('logical operators')
+// &&,||,!
+n1=100
+n2=200
+n3=150
+res = (n1<n2)&&(n2>n3)
+console.log('(n1<n2)&&(n2>n3) ',res )
+res = !((n1<n2)&&(n2>n3))
+console.log('!((n1<n2)&&(n2>n3)) ',res )
+// ?
