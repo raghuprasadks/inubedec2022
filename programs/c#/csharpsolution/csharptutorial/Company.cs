b5fcad1cb104ce59c120455da4d192bc51f62438
raghu prasad <prasadraghuks@gmail.com>
2022-12-15 14:53:39 +0530

    updated

---
+++ programs/c#/csharpsolution/csharptutorial/Company.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharptutorial
+{
+    internal class Company
+    {
+        //attributes
+        public string code;
+        public string name;
+        public string address;
+        public string location;
+        public int noofemp;
+
+        public Company()
+        {
+            Console.WriteLine("Default constructor");
+        }
+
+        public Company(string code, string name, string address, string location, int noofemp)
+        {
+            Console.WriteLine("Parameterized constructor");
+            this.code = code;
+            this.name = name;
+            this.address = address;
+            this.location = location;
+            this.noofemp = noofemp;
+            
+        }
+
+
+
+
+
+        // functionality
+        public void Information()
+        {
+            Console.WriteLine($" Name : {name} Location : {location}");
+        }
+    }
+}
--- programs/c#/csharpsolution/csharptutorial/Program.cs
+++ programs/c#/csharpsolution/csharptutorial/Program.cs
@@ -8,7 +8,29 @@ internal class Program
       //  DataTypes_1.DataTypesDemo();
       //  Operators_2.ArithmeticOperator();
 
-        UserInputs_3.UserInputDemo();
-        Console.ReadKey();
+       // UserInputs_3.UserInputDemo();
+
+        Company comp1 = new Company();
+        comp1.name = "ABC";
+        comp1.code = "1";
+        comp1.address = "JP Nagar";
+        comp1.location = "Bengaluru";
+        comp1.noofemp = 1000;
+        Console.WriteLine("Name " + comp1.name);
+        comp1.Information();
+
+        Company comp2 = new Company();
+        comp2.name = "PQR";
+        comp2.code = "2";
+        comp2.address = "White Field";
+        comp2.location = "Bengaluru";
+        comp2.noofemp = 3000;
+        Console.WriteLine("Name " + comp2.name);
+        comp2.Information();
+
+        Company comp3 = new Company("3","XYZ","Nehru Road","Hubli",200);
+        comp3.Information();
+
+       Console.ReadKey();
     }
 }
