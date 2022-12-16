9320438fee450e2560a6030d890f8165359cafd8
raghu prasad <prasadraghuks@gmail.com>
2022-12-16 13:28:18 +0530

    updated

--- programs/c#/csharpsolution/csharptutorial/CollectionsDemo.cs
+++ programs/c#/csharpsolution/csharptutorial/CollectionsDemo.cs
@@ -11,8 +11,12 @@ namespace csharptutorial
     {
         internal static void DemoCollections()
         {
+            /**
             ArrayListDemo ald = new ArrayListDemo();
             ald.demoArrayList();
+            **/
+            ListDemo listDemo = new ListDemo(); 
+            listDemo.listDemo();
         }
     }
 
@@ -28,10 +32,10 @@ namespace csharptutorial
             arrayList.Add(true);
             arrayList.Add(5.6);
             int zeroindex = (int)arrayList[0];
-            Console.WriteLine("Array element at zeroindex "+ zeroindex);
+            Console.WriteLine("Array element at zeroindex " + zeroindex);
 
             Console.WriteLine("iterate :");
-            foreach(var ele in arrayList)
+            foreach (var ele in arrayList)
             {
                 Console.WriteLine(ele);
             }
@@ -53,21 +57,58 @@ namespace csharptutorial
                 Console.WriteLine(ele);
             }
 
+        }
+    }
 
+    internal class Employees{
 
+        int code;
+        string name;
+        string desg;
+        string dept;
+        int salary;
 
+        public Employees()
+        {
 
+        }
 
-
-
-
-
-
-
-
+        public Employees(int code, string name, string desg, string dept, int salary)
+        {
+            this.code = code;
+            this.name = name;
+            this.desg = desg;
+            this.dept = dept;
+            this.salary = salary;   
 
         }
 
 
+        public string Informtion()
+        {
+            return $" Code : {this.code} Name : {this.name} Dept : {this.dept} Desg: {this.desg} Salary : {this.salary}";
+        }
     }
+
+    internal class ListDemo
+    {
+        public void listDemo()
+        {
+            List<Employees> empList = new List<Employees>();
+
+            Employees emp1 = new Employees(1, "ravi", "IT", "SE", 25000);
+            Employees emp2 = new Employees(2, "gagana", "IT", "SE", 25000);
+            Employees emp3 = new Employees(3, "satvik", "Accounts", "Executive", 35000);
+            empList.Add(emp1);
+            empList.Add(emp2);
+            empList.Add(emp3);
+
+            foreach(Employees emp in empList)
+            {
+                Console.WriteLine(emp.Informtion());
+            }
+        }
+    }
+
+
 }
