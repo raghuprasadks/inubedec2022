8a3314d9e44ba2e8438a65414c83f50c6d242978
raghu prasad <prasadraghuks@gmail.com>
2022-12-20 12:06:27 +0530

    updated

---
+++ programs/webapp/WebApplicationSolution/DOTNETWebApplication/Calculator.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="DOTNETWebApplication.Calculator" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Calculator"></asp:Label><br />
+    <asp:Label ID="Label2" runat="server" Text="Enter First Number"></asp:Label>
+    <asp:TextBox ID="TextBoxFN" runat="server"></asp:TextBox><br />
+    <asp:Label ID="Label4" runat="server" Text="Select Operation"></asp:Label>
+    
+    <asp:DropDownList ID="DropDownList1" runat="server">
+        <asp:ListItem Value="+">Addition</asp:ListItem>
+        <asp:ListItem Value="-">Subtraction</asp:ListItem>
+        <asp:ListItem Value="*">Multiplication</asp:ListItem>
+        <asp:ListItem Value="/">Division</asp:ListItem>
+    </asp:DropDownList><br />
+
+    <asp:Label ID="Label3" runat="server" Text="Enter Second Number"></asp:Label>
+    <asp:TextBox ID="TextBoxSN" runat="server"></asp:TextBox><br />
+    <asp:Button ID="ButtonCalculate" runat="server" Text="Calculate" OnClick="ButtonCalculate_Click" /><br />
+    <asp:Label ID="LabelResult" runat="server" Text=""></asp:Label>
+    
+
+</asp:Content>
---
+++ programs/webapp/WebApplicationSolution/DOTNETWebApplication/Calculator.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DOTNETWebApplication
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            int fn = Convert.ToInt32(TextBoxFN.Text);
+            string oper = DropDownList1.SelectedValue;
+            int sn = Convert.ToInt32(TextBoxSN.Text);
+            int result = 0;
+            if (oper.Equals("+"))
+            {
+                result = fn + sn;
+                LabelResult.Text = $"{fn} + {sn} = {result}";
+            }else if (oper.Equals("-"))
+            {
+                result = fn - sn;
+                LabelResult.Text = $"{fn} - {sn} = {result}";
+            }
+            else if (oper.Equals("*"))
+            {
+                result = fn * sn;
+                LabelResult.Text = $"{fn} * {sn} = {result}";
+            }
+            else if (oper.Equals("/"))
+            {
+                result = fn / sn;
+                LabelResult.Text = $"{fn} / {sn} = {result}";
+            }
+        }
+    }
+}
