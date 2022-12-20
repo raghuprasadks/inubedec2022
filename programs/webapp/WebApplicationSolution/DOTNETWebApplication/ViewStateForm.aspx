41650858d58c9d2bbf438f4259ccabecd6846213
raghu prasad <prasadraghuks@gmail.com>
2022-12-20 13:33:36 +0530

    updated

---
+++ programs/webapp/WebApplicationSolution/DOTNETWebApplication/ViewStateForm.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewStateForm.aspx.cs" Inherits="DOTNETWebApplication.ViewStateForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Sample Counter"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            <asp:Button ID="Button1" runat="server" Text="Click Me" OnClick="Button1_Click" /> <br />
+            
+        <asp:Label ID="Label2" runat="server" Text="Count"></asp:Label>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ programs/webapp/WebApplicationSolution/DOTNETWebApplication/ViewStateForm.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DOTNETWebApplication
+{
+    public partial class ViewStateForm : System.Web.UI.Page
+    {
+        //int counter = 0;
+        int ClicksCount = 1;
+
+        string msg = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Label2.Text = "Page_Load is Called";
+            if (!IsPostBack)
+            {
+                TextBox1.Text = "0";
+                msg = "Page_Load is Called:Not post back";
+            }
+            else
+            {
+                msg = "Page_Load is Called:post back";
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Label2.Text = "Button1_Click is Called";
+            /**
+            counter = counter + 1;
+            TextBox1.Text = counter.ToString();
+            Label2.Text = msg;
+            **/
+
+            if (ViewState["Clicks"] != null)
+            {
+                ClicksCount = (int)ViewState["Clicks"] + 1;
+            }
+            TextBox1.Text = ClicksCount.ToString(); ;
+            ViewState["Clicks"] = ClicksCount;
+
+        }
+    }
+}
