a113493b37bdc975442990a69e75fb6469dd0595
raghu prasad <prasadraghuks@gmail.com>
2022-12-21 13:22:49 +0530

    adonet

--- programs/c#/adonetconsole/adonetconsole/Program.cs
+++ programs/c#/adonetconsole/adonetconsole/Program.cs
@@ -13,6 +13,8 @@ namespace adonetconsole
         {
 //            CreateTable();
             InsertData();
+            ReadData();
+            DeleteData();
             Console.ReadKey();
         }
 
@@ -22,10 +24,10 @@ namespace adonetconsole
             try
             {
                 // Creating Connection  windows authetication
-                //  con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+                  con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
                 // writing sql query 
                 // sql server authentication with user id and password
-                con = new SqlConnection("Server = RAGHUPRASAD; Database = student; User Id = sa; Password = inube@123");
+                //con = new SqlConnection("Server = RAGHUPRASAD; Database = student; User Id = sa; Password = inube@123");
                 SqlCommand cm = new SqlCommand("create table student(id int not null,name varchar(100), email varchar(50), join_date date)", con);  
                 // Opening Connection  
                 con.Open();
@@ -61,6 +63,65 @@ namespace adonetconsole
                 cm.ExecuteNonQuery();
                 // Displaying a message  
                 Console.WriteLine("Record Inserted Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        static void ReadData()
+        {
+
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("select * from student", con);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+                // Iterating Data  
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["id"] + " " + sdr["name"] + " " + sdr["email"]); // Displaying Record  
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        static void DeleteData()
+        {
+
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("delete from student where id = '101'", con);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                cm.ExecuteNonQuery();
+                // Displaying a message  
+                Console.WriteLine("Record deleted Successfully");
             }
             catch (Exception e)
             {
