c5ac1cb7ff280e2812494aa53eb0360015ced86c
raghu prasad <prasadraghuks@gmail.com>
2022-12-21 18:16:34 +0530

    updated

--- programs/c#/adonetconsole/adonetconsole/Program.cs
+++ programs/c#/adonetconsole/adonetconsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,10 @@ namespace adonetconsole
         static void Main(string[] args)
         {
 //            CreateTable();
-            InsertData();
-            ReadData();
-            DeleteData();
+          //  InsertData();
+           // ReadData();
+           // DeleteData();
+            SPWithoutParam();
             Console.ReadKey();
         }
 
@@ -133,6 +135,56 @@ namespace adonetconsole
                 con.Close();
             }
         }
+
+        static void SPWithoutParam()
+        {
+                //Store the connection string in the ConnectionString variable
+                string ConnectionString = @"data source=.; database=StudentDB; integrated security=SSPI";
+
+
+             //   SqlConnection con = null;
+                try
+                {
+                    // Creating Connection  
+              //      con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+
+                    //Create the SqlConnection object
+                    using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    //Create the SqlCommand object by passing the stored procedure name and connection object as parameters
+                    SqlCommand cmd = new SqlCommand("spGetStudents", connection)
+                    {
+                        //Specify the command type as Stored Procedure
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    //Open the Connection
+                    connection.Open();
+
+                    //Execute the command i.e. Executing the Stored Procedure using ExecuteReader method
+                    //SqlDataReader requires an active and open connection
+                    SqlDataReader sdr = cmd.ExecuteReader();
+
+                    //Read the data from the SqlDataReader 
+                    //Read() method will returns true as long as data is there in the SqlDataReader
+                    while (sdr.Read())
+                    {
+                        //Accessing the data using the string key as index
+                        Console.WriteLine(sdr["Id"] + ",  " + sdr["Name"] + ",  " + sdr["Email"] + ",  " + sdr["Mobile"]);
+
+                        //Accessing the data using the integer index position as key
+                        //Console.WriteLine(sdr[0] + ",  " + sdr[1] + ",  " + sdr[2] + ",  " + sdr[3]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+            }
+            Console.ReadKey();
+        
+    
+}
 
     }
     }
