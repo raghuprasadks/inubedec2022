af824d321052094adc494bc069ed34102eef2080
raghu prasad <prasadraghuks@gmail.com>
2022-12-21 15:38:18 +0530

    updated

---
+++ programs/webapp/adonetwebsoln/adonetweb/Student.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Student.aspx.cs" Inherits="adonetweb.Student" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Student Form"></asp:Label><br />
+
+    <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+
+    <asp:TextBox ID="TextBoxName" runat="server"></asp:TextBox><br />
+    <asp:Label ID="Label3" runat="server" Text="Email"></asp:Label>
+
+    <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox><br />
+
+    <asp:Label ID="Label4" runat="server" Text="Address"></asp:Label>
+
+    <asp:TextBox ID="TextBoxAddress" runat="server"></asp:TextBox><br />
+    <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" /><br />
+
+    <asp:Label ID="LabelResult" runat="server" Text=""></asp:Label><br />
+
+    <asp:Label ID="LabelData" runat="server" Text=""></asp:Label>
+
+</asp:Content>
---
+++ programs/webapp/adonetwebsoln/adonetweb/Student.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace adonetweb
+{
+    public partial class Student : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+                // Writing insert query  
+                string query = "insert into students(name,email,address) values('" + TextBoxName.Text + "','" + TextBoxEmail.Text + "','" + TextBoxAddress.Text + "')";  
+                SqlCommand sc = new SqlCommand(query, con);
+                // Opening connection  
+                con.Open();
+                // Executing query  
+                int status = sc.ExecuteNonQuery();
+                LabelResult.Text = "Your record has been saved with the following details!";
+                // ----------------------- Retrieving Data ------------------ //  
+                SqlCommand cm = new SqlCommand("select top 1 * from students order by id desc", con);
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+                sdr.Read();
+                LabelData.Text = " ";
+                LabelData.Text += $"User Name  {sdr["name"].ToString()}";
+               // Label3.Text = "Email ID"; Label6.Text = sdr["email"].ToString();
+                //Label4.Text = "Contact"; Label7.Text = sdr["contact"].ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("OOPs, something went wrong." + ex);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
+    
