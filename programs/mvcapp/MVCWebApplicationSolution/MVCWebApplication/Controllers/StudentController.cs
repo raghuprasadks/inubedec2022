3b7c00ea4d3028501c3ce400c4d27acd89962ffc
raghu prasad <prasadraghuks@gmail.com>
2022-12-22 12:14:16 +0530

    updated

---
+++ programs/mvcapp/MVCWebApplicationSolution/MVCWebApplication/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        /**
+        public ActionResult Index()
+        {
+            return View();
+        }
+    **/
+    public string Index()
+        {
+            return "Student Controller:Index";
+        }
+    }
+}
---
+++ programs/mvcapp/MVCWebApplicationSolution/MVCWebApplication/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApplication.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Deptartment { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
