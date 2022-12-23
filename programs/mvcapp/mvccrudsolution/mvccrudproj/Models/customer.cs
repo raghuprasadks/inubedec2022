355223826f9123c13f364f55ccbfba61683d453a
raghu prasad <prasadraghuks@gmail.com>
2022-12-23 16:37:02 +0530

    updated

---
+++ programs/mvcapp/mvccrudsolution/mvccrudproj/Models/customer.cs
@@ -0,0 +1,27 @@
+namespace mvccrudproj.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string email { get; set; }
+
+        public long mobile { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string location { get; set; }
+    }
+}
