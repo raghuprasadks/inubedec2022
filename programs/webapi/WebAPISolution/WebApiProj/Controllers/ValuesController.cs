95b989df781a1851600ee4f9119b5dd19368e248
raghu prasad <prasadraghuks@gmail.com>
2022-12-26 12:53:14 +0530

    updated

---
+++ programs/webapi/WebAPISolution/WebApiProj/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiProj.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value: ID "+id;
+        }
+
+        // GET api/values/5
+        public string Get(string name,string city)
+        {
+            return "Name: " + name + " City :" + city;
+        }
+
+
+
+        // POST api/values
+        public string Post([FromBody]string value)
+        {
+            return value;
+        }
+
+        // PUT api/values/5
+        public string Put(int id, [FromBody]string value)
+        {
+            return "updated data for id " + id + " value " + value;
+        }
+
+        // DELETE api/values/5
+        public string Delete(int id)
+        {
+            return "Deleted data with id " + id;
+        }
+    }
+}
