44d4aaea1e20236acc229e23c4cb011ae58a79c1
raghu prasad <prasadraghuks@gmail.com>
2022-12-26 13:40:29 +0530

    updated

---
+++ programs/webapi/WebAPISolution/WebApiProj/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiProj.Models;
+
+namespace WebApiProj.Controllers
+{
+    public class ProductController : ApiController
+    {
+
+        Product[] products = new Product[]
+        {
+            new Product { id = 1, name = "Tomato Soup",desc = "Tomato Soup desc", supplier = "Groceries supplier", price = 1 },
+            new Product { id = 2, name = "Yo-yo",desc = "Yo-yo desc", supplier = "Toys supplier", price = 3 },
+            new Product { id = 3, name = "Hammer",desc = "Hammer desc", supplier = "Hardware supplier", price = 16 }
+        };
+        // GET: api/Product
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        // GET: api/Product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        public void Post(Product product)
+        {
+
+        }
+
+        // PUT: api/Product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ programs/webapi/WebAPISolution/WebApiProj/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiProj.Models
+{
+    
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string supplier { get; set; }
+        public int price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(int id,string name,string desc,string supplier,int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.desc = desc;
+            this.supplier = supplier;
+            this.price = price;
+        }
+
+    }
+
+
+}
