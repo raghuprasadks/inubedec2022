2a417e6489cee7673cd2561eb8e779523234b72b
raghu prasad <prasadraghuks@gmail.com>
2022-12-27 17:40:13 +0530

    updated

---
+++ programs/netcore/netcoresolution/netcoreproject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcoreproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
