211c4595de3ed09c91a6efc535125a71954f994b
raghu prasad <prasadraghuks@gmail.com>
2022-12-28 16:47:09 +0530

    updated

--- programs/react/basicreactapp/src/App.js
+++ programs/react/basicreactapp/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div>
       <h1>Welcome to React</h1>
+      <Header/>
     </div>
   );
 }
---
+++ programs/react/basicreactapp/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header=()=>{
+    return(
+        <h2>This is a header</h2>
+    );
+}
+
+export default Header;
