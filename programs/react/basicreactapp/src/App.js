9a2ed5d903a926211a736c9640521741b74b622d
raghu prasad <prasadraghuks@gmail.com>
2022-12-28 18:15:40 +0530

    updated

--- programs/react/basicreactapp/src/App.js
+++ programs/react/basicreactapp/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Header from './Header';
 import Content from './Content';
 import DemoForm from './components/DemoForm';
+import SimpleInterest from './components/SimpleInterest';
 
 function App() {
   return (
     <div>
       <h1>Welcome to React</h1>
+      {/**
       <Header/>
       <Content/>
       <DemoForm></DemoForm>
-            </div>
+ */}
+      <SimpleInterest/>
+      </div>
   );
 }
 
